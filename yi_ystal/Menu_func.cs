683f864878fd7536052085a3d01dd2d13413bd01
EduardWer <eduard.unanov66@mail.ru>
2023-11-09 11:09:59 +0000

    Initial commit

---
+++ yi_ystal/Menu_func.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ia_ystal
+{
+    internal class Menus
+    {
+        public int min;
+        public int max;
+        private int CursorPos;
+        private ConsoleKeyInfo key;
+
+        public Menus(int Min, int Max)
+        {
+            min = Min;
+            max = Max;
+        }
+
+        public int Menu()
+        {
+            CursorPos = min;
+            while(true)
+            {
+                Console.SetCursorPosition(0, CursorPos);
+                Console.Write("->");
+                Console.SetCursorPosition(0,0);
+                key = Console.ReadKey();
+                Console.SetCursorPosition(0, CursorPos);
+                Console.Write("  ");
+                if (key.Key == ConsoleKey.UpArrow && CursorPos > min)
+                {
+                    CursorPos--;
+                }
+                if (key.Key == ConsoleKey.DownArrow && CursorPos < max)
+                {
+                    CursorPos++;
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    return CursorPos;
+                }
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
---
+++ yi_ystal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ia_ystal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int CursorPos;
+            string path;
+            List<DriveInfo> drives = new List<DriveInfo>();
+            string[] files;
+            Menus menus = new Menus(0,0);
+            while (true)
+            {
+                drives = Explorer.GetDrives();
+                menus = new Menus(2, drives.Count + 1);
+                CursorPos = menus.Menu();
+                path = drives[CursorPos - 2].Name;
+                do
+                {
+                    
+                    Explorer.GetFiles(path);
+
+                }
+                while (CursorPos != 0);
+            }                                                                                                                                                                                                                                                                                                                                                                                       //Сделал Эдуард
+            
+
+        }
+    }
+}
